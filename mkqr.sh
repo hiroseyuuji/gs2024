efac8272877989ff70d902ff896f0a9eb76d4b73
HIROSE Yuuji <yuuji@gentei.org>
2023-07-02 13:40:57 +0859

    Separate "do"

--- mkqr.sh
+++ mkqr.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-paste -d, meibo.csv jumon.csv | while IFS=, read id kanji hiragana jumon; do
+paste -d, meibo.csv jumon.csv | while IFS=, read id kanji hiragana jumon
+do
   qrencode -o out/$id.png "$jumon"
 done
